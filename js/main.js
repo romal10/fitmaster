730134a54b7f89867f8468d822923d62b68c89bf
Mohaiyudin Romal <45965609+romal10@users.noreply.github.com>
2024-10-16 15:42:08 +0200

    Barfiller

--- js/main.js
+++ js/main.js
@@ -132,12 +132,24 @@
     });
 
     /*------------------
-        Info Page HistoryStart 
+        Bar Filler Start 
     --------------------*/
 
+    document.addEventListener("DOMContentLoaded", function() {
+        const fills = document.querySelectorAll('.progress-container .fill');
+    
+        fills.forEach(fill => {
+            const percentage = fill.style.width;
+            fill.style.width = '0'; // Reset width
+            setTimeout(() => {
+                fill.style.width = percentage; // Animate to target width
+            }, 100); // Delay to allow transition to be visible
+        });
+    });
+    
     
     /*------------------
-        Info Page end
+        Bar Filler end
     --------------------*/
     /*------------------
         Image Popup
